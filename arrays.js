8563b51d62ca0ce6c14598ac753d45ea37ea34ce
nixonbii2@gmail.com <nixonbii2@gmail.com>
2018-07-26 23:37:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -46,6 +46,6 @@ function destructivelyRemoveElementFromEndOfArray(iceCreams) {
 }
 
 function removeElementFromEndOfArray(iceCreams) {
-  iceCreams.slice(-1);
+  iceCreams.slice(0, iceCreams.length - 1);
   return iceCreams;
 }
